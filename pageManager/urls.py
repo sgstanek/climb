d668d31c9b44baa0058f25e2560558e2eed2490c
sgstanek <samuelstanek.email@gmail.com>
2023-06-03 13:58:27 -0500

    accounts

--- pageManager/urls.py
+++ pageManager/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-
 from . import views  # Import your app's views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Create a route for the index page of your app
     path('', views.index, name='index'),
+    path('accounts/login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/profile/', views.profile, name='profile'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pageManager/views.py
+++ pageManager/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
 from dataManager.models import Location
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     locations = Location.objects.all()
     return render(request, 'index.html', {'locations': locations})
 
+def register(request):  
+   if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+         user = form.save()
+         login(request, user)
+         return redirect('index')
+   else:
+      form = UserCreationForm()
+   return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html', {'user': request.user})
