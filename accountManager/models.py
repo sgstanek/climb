4a0d8067d8b1021f47552d82f7e8169510b5cbd6
sgstanek <samuelstanek.email@gmail.com>
2023-06-04 14:52:56 -0500

    fixes

--- accountManager/models.py
+++ accountManager/models.py
@@ -11,13 +11,13 @@ from django.dispatch import receiver
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-	displayName = models.CharField(max_length=150, default=user.name)
-	profilePicture = models.ImageField(upload_to='files/profilePicture')
-	bio = models.CharField(max_length=200)
+	displayName = models.CharField(max_length=150, blank=True, null=True)
+	profilePicture = models.ImageField(upload_to='files/profilePicture',blank=True, null=True)
+	bio = models.CharField(max_length=200,blank=True, null=True)
 
-	favoritedRoutes = models.ManyToManyField(Route, related_name='favoritedRoutes')
-	completedRoutes = models.ManyToManyField(Route, related_name='completedRoutes')
+	favoritedRoutes = models.ManyToManyField(Route, related_name='favoritedRoutes',blank=True)
+	completedRoutes = models.ManyToManyField(Route, related_name='completedRoutes',blank=True)
 
 @receiver(user_signed_up)
 def user_signed_up_(request, user, **kwargs):
-    Profile.objects.create(user=user)
+    Profile.objects.create(user=user, displayName=user.username)
